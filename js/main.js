7dc055201db69781d7092d169fedf576dd95bc03
DOMIUS-CORE <murderman356@gmail.com>
2026-07-13 23:12:29 +0500

    print() -> documet.write console.log+ let __ = __ if, else if, else switch (){ case_: } let __ = [] wile for () {}

---
+++ js/main.js
@@ -0,0 +1,128 @@
+/*
+document.write('js say hi');
+//ставит текст после script
+*/
+/*
+//ставит информацию в консоль в console разработчик
+console.log('js cay hi in console');
+console.info('js cay hi in console');
+console.error('js cay error in console');
+console.warn('js cay warning in console');
+*/
+/*
+var let const #%T%^.... не использовать, 1234..... не начинать переменную так, можно $ _ заглавные буквы
+var устарел и его можно изменить где угодно
+let современный можно поменять
+const не меняется
+*/
+/*
+//переменные не устанавливают строгую типизацию это на автомате
+let num;
+let words;
+let bool;
+num =  10;
+words = "words";
+bool = true;
+console.log("Переменная num: " + num  + ".\n" + "Переменная words: " + words + ".\n" + "Переменная bool: " + bool + ".");
+//строки можно свободно менять с 1-го типа данных на другой
+let change = true;
+console.log("change = " + change);
+change = 20;
+console.log("change = " + change);
+//console.log("math = " + (num + - * / %  change));
+console.log("math = " + (num +  change));
+//даже текст можно в числе если нет букв + Math.max() есть и другие, это математические функции Math.PI Math.Const() и тд https://mathjs.org/
+let numb = Number("12");
+console.log("numb = " + Math.max(numb));
+*/
+/*
+//при проверке есть разница сравнений
+// ===
+// 5 === 5             true (число === число)
+// 5 === "5"           false (число !== строка)
+// true === 1          false (boolean !== number)
+// null === undefined  false
+// ==
+// 5 == "5"            true (строка "5" стала числом 5)
+// true == 1           true (true стал 1)
+// false == 0          true (false стал 0)
+// null == undefined   true
+// "" == false         true (пустая строка стала 0, false тоже 0)
+// проверка || "ИЛИ", && "И" просто название переменной проверка на true на false можно !test2
+let test = Number(60);
+let test2 = true;
+if (test === 60 && test2) {
+    console.log('yep if = 60');
+} else if (test < 60) {
+    console.log('no if < 60');
+} else if (test > 60) {
+    console.log('hm if > 60')
+} else {
+    console.log('error')
+}
+*/
+/*
+//case также без изменений
+let test = 5;
+
+switch (test) {
+    case 1:
+        console.log("test = 1");
+        break;
+    case 2:
+        console.log("test = 2");
+        break;
+    case 3:
+        console.log("test = 3");
+        break;
+    case 4:
+        console.log("test = 4");
+        break;
+    case 5:
+        console.log("test = 5");
+        break;
+    default:
+        console.log("default")
+}
+*/
+/*
+// var старая сейчас const добавлять можно перезаписывать нет let изменяемый, любые данные, arr.length длинна,
+let arr = [5, true, "apple", 0.4, 0, -100];
+arr[2] = "not-apple";
+console.log(arr);
+console.log(arr.length);
+let matrix = [
+    [1, 2, 3], ["a", "b", "c"], [true, false, false]
+];
+console.log(matrix);
+*/
+/*
+// циклы простой бесконечный и 1-н раз сделает и потом проверит значения
+for(let i = 0; i < 10; i += 1) {
+    console.log(i);
+}
+let j = 100;
+while (j > 0) {
+    console.log(j);
+    j -= 1;
+}
+// let onlyTrue = true;
+// while (onlyTrue) {}
+do {
+    console.log(j)
+    j++
+} while (j <= 50)
+let test3 = [["name1", true], ["name2", true], ["name3", false], ["name4", true], ["name5", false]]
+for (let k = 0; k < test3.length; k++) {
+    console.log("k=" + k);
+    if (test3[k][0] === "name4") {
+        console.log("test3[" + k + "][0] = name4")
+        break;
+    }
+    if (test3[k][1] === true) {
+        console.log(test3[k][0]);
+    } else {
+        console.log(test3[k][0] + " its false")
+    }
+}
+*/
